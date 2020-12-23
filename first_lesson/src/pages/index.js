43635aabadc12f25cc694f1fff2d6a60eab4b0ef
yuugure <westhillworker@gmail.com>
2020-12-23 13:00:17 +0900

    first lesson

--- first_lesson/src/pages/index.js
+++ first_lesson/src/pages/index.js
@@ -1,34 +1,12 @@
 import * as React from "react"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-
-// markup
-const IndexPage = () => {
+const Index = () => {
   return (
-    <main style={pageStyles}>
-      <title>First Lesson Start</title>
-      <h1 style={headingStyles}>
-        初めてのGatsby学習!!
-        <br />
-        <span style={headingAccentStyles}>楽しく学習していきましょう</span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-    </main>
+    <div style={{ color: `purple` }}>
+      <h1>Gatsby学習を始めよう!</h1>
+      <p>まずは簡単なPagesの表示から</p>
+    </div>
   )
 }
 
-export default IndexPage
+export default Index
